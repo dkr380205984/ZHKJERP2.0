8dfb19c3162c62ff1ff9326c43cc9c8b5b02db12
dkr380205984 <380205984@qq.com>
2019-10-29 15:59:35 +0800

    input组件完成 , http状态码重写

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import zhInput from './zhInput/zhInput.vue'
+export default {
+  install: function (vue) {
+    vue.component('zhInput', zhInput)
+  }
+}
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,40 @@
 <template>
-  <div id="login"></div>
+  <div id="login">
+    <zh-input v-model="msg"
+      placeholder="输入数字测试"
+      type="number"
+      clearable
+      minLength="5"
+      maxLength="10"
+      @change="test">
+      <template slot="prepend">
+        前置元素
+      </template>
+    </zh-input>
+  </div>
 </template>
 
 <script>
+import { login } from '@/assets/js/api.js'
 export default {
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    test () {
+      console.log(this.msg)
+    }
+  },
+  mounted () {
+    login({
+      telephone: '17764530174',
+      password: '123456'
+    }).then((res) => {
 
+    })
+  }
 }
 </script>
 
