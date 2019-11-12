d3529e977612eaedd04f3c4d2d6c1dd180a55462
ye-haowen <18397946275@163.com>
2019-11-12 13:42:22 +0800

    2019-11-12-13-42

--- src/views/sample/sampleCreate.vue
+++ src/views/sample/sampleCreate.vue
@@ -4,6 +4,7 @@
     <div class="module">
       <div class="titleCtn">
         <span class="title">大身信息</span>
+        <span class="content">{{product_code_cmp}}</span>
       </div>
       <div class="editCtn">
         <div class="rowCtn">
@@ -45,21 +46,25 @@
             </span>
           </div>
         </div>
-        <div class="rowCtn">
+        <div class="rowCtn"
+          v-for="(itemIngred,indIngred) in ingredient"
+          :key="indIngred + 'ingredient'">
           <div class="colCtn flex3">
-            <span class="label">
+            <span class="label"
+              v-if="indIngred === 0">
               <span class="text">样品成分</span>
             </span>
             <span class="content">
               <el-autocomplete class="inline-input"
-                v-model="ingredient[0].ingredient_name"
+                v-model="itemIngred.ingredient_name"
                 :fetch-suggestions="searchIngredient"
                 placeholder="请输入样品成分">
               </el-autocomplete>
             </span>
           </div>
           <div class="colCtn flex3">
-            <span class="label">
+            <span class="label"
+              v-if="indIngred === 0">
               <span class="text"></span>
             </span>
             <span class="content">
@@ -67,23 +72,30 @@
                 errorPosition="bottom"
                 errorMsg="请输入数字,并保证成分总值为100%!"
                 placeholder="请输入成分百分比"
-                v-model="ingredient[0].ingredient_value">
+                v-model="itemIngred.ingredient_value">
                 <template slot="append">
                   <span>%</span>
                 </template>
               </zh-input>
             </span>
             <div class="editBtn addBtn"
-              @click="addIngredient">添加</div>
+              v-if="indIngred === 0"
+              @click="addIngredient(ingredient)">添加</div>
+            <div class="editBtn deleteBtn"
+              v-else
+              @click="deleteIngredient(ingredient,indIngred)">删除</div>
           </div>
         </div>
-        <div class="rowCtn">
+        <div class="rowCtn"
+          v-for="(itemSize,indSize) in size"
+          :key="indSize +'size'">
           <div class="colCtn">
-            <span class="label">
+            <span class="label"
+              v-if="indSize === 0">
               <span class="text">样品规格</span>
             </span>
             <span class="content">
-              <el-select v-model="size[0].size"
+              <el-select v-model="itemSize.size"
                 placeholder="请选择规格">
                 <el-option v-for="item in sizeArr"
                   :key="item.value"
@@ -94,11 +106,12 @@
             </span>
           </div>
           <div class="colCtn">
-            <span class="label">
+            <span class="label"
+              v-if="indSize === 0">
               <span class="text"></span>
             </span>
             <span class="content">
-              <zh-input v-model="size[0].weight"
+              <zh-input v-model="itemSize.weight"
                 type="number"
                 placeholder="请输入克重">
                 <template slot="append">
@@ -108,33 +121,47 @@
             </span>
           </div>
           <div class="colCtn">
-            <span class="label">
+            <span class="label"
+              v-if="indSize === 0">
               <span class="text"></span>
             </span>
             <span class="content">
-              <zh-input v-model="size[0].desc"
+              <zh-input v-model="itemSize.desc"
                 placeholder="请输入尺寸信息">
                 <template slot="append">
                   <span>cm</span>
                 </template>
               </zh-input>
             </span>
-            <div class="editBtn addBtn">添加</div>
+            <div class="editBtn addBtn"
+              v-if="indSize === 0"
+              @click="addSize(size)">添加</div>
+            <div class="editBtn deleteBtn"
+              v-else
+              @click="deleteSize(size,indSize)">删除</div>
           </div>
         </div>
-        <div class="rowCtn">
+        <div class="rowCtn"
+          v-for="(itemColor,indColor) in color"
+          :key="indColor + 'color'">
           <div class="colCtn flex3">
-            <span class="label">
+            <span class="label"
+              v-if="indColor === 0">
               <span class="text">样品配色组</span>
             </span>
             <span class="content">
               <el-autocomplete class="inline-input"
-                v-model="color[0].color"
+                v-model="itemColor.color"
                 :fetch-suggestions="searchColor"
                 placeholder="请输入配色信息">
               </el-autocomplete>
             </span>
-            <div class="editBtn addBtn">添加</div>
+            <div class="editBtn addBtn"
+              v-if="indColor === 0"
+              @click="addColour(color,false)">添加</div>
+            <div class="editBtn deleteBtn"
+              v-else
+              @click="deleteColour(color,indColor)">删除</div>
           </div>
         </div>
       </div>
@@ -149,6 +176,177 @@
           inactive-color="#dcdfe6">
         </el-switch>
       </div>
+      <template v-if="hasFitting">
+        <div class="editCtn"
+          v-for="(itemFitting,indFitting) in fittingInfo"
+          :key="indFitting">
+          <span class="titleNum">配件{{chinaNum[indFitting]}}</span>
+          <span class="deleteIcon el-icon-close"
+            @click="deleteFitting(indFitting)"></span>
+          <div class="rowCtn">
+            <div class="colCtn flex3">
+              <span class="label">
+                <span class="text">配件名称</span>
+              </span>
+              <span class="content">
+                <zh-input placeholder="请输入配件名称"
+                  v-model="itemFitting.fitting_name" />
+              </span>
+            </div>
+            <div class="colCtn flex3">
+              <span class="label">
+                <span class="text">配件分类</span>
+              </span>
+              <span class="content">
+                <el-cascader v-model="itemFitting.type"
+                  placeholder="请选择配件分类"
+                  :options="treeData"
+                  :props="{ expandTrigger: 'hover' }">
+                </el-cascader>
+              </span>
+            </div>
+          </div>
+          <div class="rowCtn"
+            v-for="(itemColor,indColor) in itemFitting.color"
+            :key="indColor + 'color'">
+            <div class="colCtn flex3">
+              <span class="label"
+                v-if="indColor === 0">
+                <span class="text">配件色组</span>
+              </span>
+              <span class="content">
+                <el-autocomplete class="inline-input"
+                  v-model="itemColor.color"
+                  :fetch-suggestions="searchIngredient"
+                  placeholder="请选择对应色组">
+                </el-autocomplete>
+              </span>
+            </div>
+            <div class="colCtn flex3">
+              <span class="label"
+                v-if="indColor === 0">
+                <span class="text">配件数量</span>
+              </span>
+              <span class="content">
+                <zh-input type="number"
+                  errorPosition="bottom"
+                  errorMsg="请输入数字"
+                  placeholder="请输入配件数量"
+                  v-model="itemColor.fitting_number">
+                  <template slot="append">
+                    <span>个</span>
+                  </template>
+                </zh-input>
+              </span>
+              <div class="editBtn addBtn"
+                v-if="indColor === 0"
+                @click="addColour(itemFitting.color,true)">添加</div>
+              <div class="editBtn deleteBtn"
+                v-else
+                @click="deleteColour(itemFitting.color,indColor)">删除</div>
+            </div>
+          </div>
+          <div class="rowCtn"
+            v-for="(itemIngred,indIngred) in itemFitting.ingredient"
+            :key="indIngred + 'ingred'">
+            <div class="colCtn flex3">
+              <span class="label"
+                v-if="indIngred === 0">
+                <span class="text">配件成分</span>
+                <span class="explanation">(选填)</span>
+              </span>
+              <span class="content">
+                <el-autocomplete class="inline-input"
+                  v-model="itemIngred.ingredient_name"
+                  :fetch-suggestions="searchIngredient"
+                  placeholder="请选择成分">
+                </el-autocomplete>
+              </span>
+            </div>
+            <div class="colCtn flex3">
+              <span class="label"
+                v-if="indIngred === 0">
+                <span class="text"></span>
+              </span>
+              <span class="content">
+                <zh-input type="number"
+                  errorPosition="bottom"
+                  errorMsg="请输入数字,并保证成分总值为100%!"
+                  placeholder="请输入成分百分比"
+                  v-model="itemIngred.ingredient_value">
+                  <template slot="append">
+                    <span>%</span>
+                  </template>
+                </zh-input>
+              </span>
+              <div class="editBtn addBtn"
+                v-if="indIngred === 0"
+                @click="addIngredient(itemFitting.ingredient)">添加</div>
+              <div class="editBtn deleteBtn"
+                v-else
+                @click="deleteIngredient(itemFitting.ingredient,indIngred)">删除</div>
+            </div>
+          </div>
+          <div class="rowCtn"
+            v-for="(itemSize,indSize) in itemFitting.size"
+            :key="indSize + 'size'">
+            <div class="colCtn flex3">
+              <span class="label"
+                v-if="indSize === 0">
+                <span class="text">配件规格</span>
+                <span class="explanation">(选填)</span>
+              </span>
+              <span class="content">
+                <el-select v-model="itemSize.size"
+                  placeholder="请选择规格">
+                  <el-option v-for="item in sizeArr"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option>
+                </el-select>
+              </span>
+            </div>
+            <div class="colCtn flex3">
+              <span class="label"
+                v-if="indSize === 0">
+                <span class="text"></span>
+              </span>
+              <span class="content">
+                <zh-input v-model="itemSize.weight"
+                  type="number"
+                  placeholder="请输入克重">
+                  <template slot="append">
+                    <span>g</span>
+                  </template>
+                </zh-input>
+              </span>
+            </div>
+            <div class="colCtn flex3">
+              <span class="label"
+                v-if="indSize === 0">
+                <span class="text"></span>
+              </span>
+              <span class="content">
+                <zh-input v-model="itemSize.desc"
+                  placeholder="请输入尺寸信息">
+                  <template slot="append">
+                    <span>cm</span>
+                  </template>
+                </zh-input>
+              </span>
+              <div class="editBtn addBtn"
+                v-if="indSize === 0"
+                @click="addSize(itemFitting.size)">添加</div>
+              <div class="editBtn deleteBtn"
+                v-else
+                @click="deleteSize(itemFitting.size,indSize)">删除</div>
+            </div>
+          </div>
+        </div>
+        <div class="btn btnWhiteBlue add_fitting_btn"
+          @click="addFitting">添加配件</div>
+      </template>
     </div>
     <div class="module">
       <div class="titleCtn">
@@ -208,6 +406,7 @@
 </template>
 <script>
 import { productType, flower, ingredient, colour, getToken } from '@/assets/js/api.js'
+import { chinaNum } from '@/assets/js/dictionary.js'
 export default {
   data () {
     return {
@@ -228,7 +427,18 @@ export default {
       desc: '',
       postData: { token: '' },
       fileArr: [],
-      hasFitting: false
+      hasFitting: false,
+      fittingInfo: [{
+        fitting_name: '',
+        type: [],
+        color: [{ color: '', fitting_number: '' }],
+        ingredient: [{
+          ingredient_name: '',
+          ingredient_value: ''
+        }],
+        size: [{ size: '', weight: '', desc: '' }]
+      }],
+      chinaNum: chinaNum
     }
   },
   methods: {
@@ -243,26 +453,66 @@ export default {
     beforeAvatarUpload () {
 
     },
-    addIngredient () {
-      this.ingredient.push({
+    addIngredient (item) {
+      item.push({
         ingredient_name: '',
         ingredient_value: ''
       })
     },
-    deleteIngredient (index) {
-      this.ingredient.splice(index, 1)
+    deleteIngredient (item, index) {
+      item.splice(index, 1)
     },
-    addSize () {
-
+    addSize (item) {
+      console.log('size', item)
+      item.push({ size: '', weight: '', desc: '' })
     },
-    deleteSize () {
-
+    deleteSize (item, index) {
+      item.splice(index, 1)
     },
-    addColour () {
-
+    addColour (item, hasNum) {
+      if (hasNum) {
+        item.push({ color: '', fitting_number: '' })
+      } else {
+        item.push({ color: '' })
+      }
     },
-    deleteColour () {
-
+    deleteColour (item, index) {
+      item.splice(index, 1)
+    },
+    addFitting () {
+      this.fittingInfo.push({
+        fitting_name: '',
+        type: [],
+        color: [{ color: '', fitting_number: '' }],
+        ingredient: [{
+          ingredient_name: '',
+          ingredient_value: ''
+        }],
+        size: [{ size: '', weight: '', desc: '' }]
+      })
+    },
+    deleteFitting (index) {
+      this.$confirm('此操作将删除该配件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.fittingInfo.splice(index)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消'
+        })
+      })
+    }
+  },
+  computed: {
+    product_code_cmp () {
+      return '19ABA0001'
     }
   },
   mounted () {
