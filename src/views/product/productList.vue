8976fc6d4ec68a4355848ba5c4efcb8b845fbf6d
dkr380205984 <380205984@qq.com>
2019-11-15 14:51:51 +0800

    产品列表完成

--- src/views/product/productList.vue
+++ src/views/product/productList.vue
@@ -23,7 +23,8 @@
               @change="getDate">
             </el-date-picker>
             <div class="btn btnGray"
-              style="margin-left:0">重置</div>
+              style="margin-left:0"
+              @click="reset">重置</div>
           </div>
           <div class="rightCtn">
           </div>
@@ -48,14 +49,38 @@
                   <el-cascader class="filter"
                     placeholder="筛选品类"
                     :options="typeArr"
+                    @change="getType"
                     clearable
                     filterable>
                   </el-cascader>
                 </div>
               </transition>
             </div>
-            <div class="col"
-              style="flex:0.5"><span class="text">花型</span></div>
+            <div class="col">
+              <span class="text">
+                <transition v-show="!searchFlowerFlag"
+                  name="el-zoom-in-bottom">
+                  <span class="text">花型
+                    <i class="el-icon-search iconBtn"
+                      @click="searchFlowerFlag=true"></i>
+                  </span>
+                </transition>
+                <transition name="el-zoom-in-top">
+                  <div v-show="searchFlowerFlag"
+                    class="filterBox">
+                    <el-select v-model="flower"
+                      clearable
+                      placeholder="筛选花型">
+                      <el-option v-for="(item,index) in flowerArr"
+                        :key="index"
+                        :label="item.name"
+                        :value="item.id">
+                      </el-option>
+                    </el-select>
+                  </div>
+                </transition>
+              </span>
+            </div>
             <div class="col"><span class="text">名称</span></div>
             <div class="col"><span class="text middle">图片</span></div>
             <div class="col"><span class="text">创建人</span></div>
@@ -67,7 +92,34 @@
                 </span>
               </span>
             </div>
-            <div class="col"><span class="text">状态</span></div>
+            <div class="col">
+              <span class="text">
+                <transition v-show="!searchStateFlag"
+                  name="el-zoom-in-bottom">
+                  <span class="text">状态
+                    <i class="el-icon-search iconBtn"
+                      @click="searchStateFlag=true"></i>
+                  </span>
+                </transition>
+                <transition name="el-zoom-in-top">
+                  <div v-show="searchStateFlag"
+                    class="filterBox">
+                    <el-select v-model="state"
+                      clearable
+                      style="height:32px"
+                      multiple
+                      @change="getState"
+                      placeholder="筛选状态">
+                      <el-option v-for="(item,index) in stateArr"
+                        :key="index"
+                        :label="item.name"
+                        :value="item.value">
+                      </el-option>
+                    </el-select>
+                  </div>
+                </transition>
+              </span>
+            </div>
             <div class="col"><span class="text">操作</span></div>
           </div>
           <div class="row"
@@ -76,8 +128,7 @@
             <div class="col">{{item.product_code}}</div>
             <div class="col"
               style="flex:1.5">{{item.category_info.product_category + ' / ' + item.type_name + ' / ' + item.style_name}}</div>
-            <div class="col"
-              style="flex:0.5">{{item.flower_id}}</div>
+            <div class="col">{{item.flower_id}}</div>
             <div class="col">{{item.sample_title?item.sample_title:'无'}}</div>
             <div class="col">
               <zh-img-list :list="item.img"></zh-img-list>
@@ -160,13 +211,35 @@
 </template>
 
 <script>
-import { product } from '@/assets/js/api.js'
+import { product, productType, flower } from '@/assets/js/api.js'
 import { getHash } from '@/assets/js/common.js'
 export default {
   data () {
     return {
       searchTypeFlag: false,
+      searchFlowerFlag: false,
+      searchStateFlag: false,
       typeArr: [],
+      type: [],
+      category_id: '',
+      type_id: '',
+      style_id: '',
+      flowerArr: [],
+      flower: '',
+      stateArr: [{
+        name: '有工艺单',
+        value: 'has_craft'
+      }, {
+        name: '有配料单',
+        value: 'has_plan'
+      }, {
+        name: '有报价单',
+        value: 'has_quotation'
+      }],
+      state: [],
+      has_plan: '',
+      has_craft: '',
+      has_quotation: '',
       loading: true,
       list: [],
       keyword: '',
@@ -204,10 +277,13 @@ export default {
   },
   watch: {
     page (newVal) {
-      this.$router.push('/product/productList/page=' + this.page + '&&keyword=' + this.keyword + '&&date=' + this.date)
+      this.changeRouter(newVal)
     },
     keyword (newVal) {
-      this.$router.push('/product/productList/page=1' + '&&keyword=' + this.keyword + '&&date=' + this.date)
+      this.changeRouter()
+    },
+    flower (newVal) {
+      this.changeRouter()
     },
     $route (newVal) {
       // 点击返回的时候更新下筛选条件
@@ -222,11 +298,17 @@ export default {
         product_code: this.keyword,
         limit: 10,
         page: this.page,
+        category_id: this.category_id,
+        type_id: this.type_id,
+        style_id: this.style_id,
+        flower_id: this.flower,
+        has_plan: this.has_plan,
+        has_craft: this.has_craft,
+        has_quotation: this.has_quotation,
         type: 1,
         start_time: (this.date && this.date.length > 0) ? this.date[0] : '',
         end_time: (this.date && this.date.length > 0) ? this.date[1] : ''
       }).then((res) => {
-        console.log(res)
         this.list = res.data.data
         this.total = res.data.meta.total
         this.loading = false
@@ -242,15 +324,73 @@ export default {
       } else {
         this.date = ''
       }
+      this.category_id = params.category_id ? params.category_id : ''
+      this.type_id = params.type_id ? params.type_id : ''
+      this.style_id = params.style_id ? params.style_id : ''
+      this.flower = params.flower_id ? Number(params.flower_id) : ''
+      this.has_plan = params.has_plan ? params.has_plan : ''
+      this.has_craft = params.has_craft ? params.has_craft : ''
+      this.has_quotation = params.has_quotation ? params.has_quotation : ''
     },
     // 这里不直接监听date是防止监听事件触发两次
     getDate () {
-      this.$router.push('/product/productList/page=1' + '&&keyword=' + this.keyword + '&&date=' + this.date)
+      this.changeRouter()
+    },
+    getType (type) {
+      if (type.length === 3) {
+        this.category_id = type[0]
+        this.type_id = type[1]
+        this.style_id = type[2]
+      } else {
+        this.category_id = ''
+        this.type_id = ''
+        this.style_id = ''
+      }
+      this.changeRouter()
+    },
+    getState (state) {
+      this.has_plan = state.find((item) => item === 'has_plan') ? 1 : ''
+      this.has_craft = state.find((item) => item === 'has_craft') ? 1 : ''
+      this.has_quotation = state.find((item) => item === 'has_quotation') ? 1 : ''
+      this.changeRouter()
+    },
+    changeRouter (page) {
+      let pages = page || 1
+      this.$router.push('/product/productList/page=' + pages + '&&keyword=' + this.keyword + '&&date=' + this.date + '&&category_id=' + this.category_id + '&&type_id=' + this.type_id + '&&style_id=' + this.style_id + '&&flower_id=' + this.flower + '&&has_plan=' + this.has_plan + '&&has_craft=' + this.has_craft + '&&has_quotation=' + this.has_quotation)
+    },
+    reset () {
+      this.$router.push('/product/productList/page=&&keyword=&&date=&&category_id=&&type_id=&&style_id=&&flower_id=&&has_plan=&&has_craft=&&has_quotation=')
     }
   },
   created () {
     this.getFilters()
     this.getList()
+    Promise.all([
+      productType.list(),
+      flower.list()
+    ]).then((res) => {
+      this.typeArr = res[0].data.data.map((item) => {
+        return {
+          value: item.id,
+          label: item.name,
+          sizeArr: item.sizeArr,
+          child_size: item.child_size,
+          children: item.child.length === 0 ? null : item.child.map((item) => {
+            return {
+              value: item.id,
+              label: item.name,
+              children: item.child.length === 0 ? null : item.child.map((item) => {
+                return {
+                  value: item.id,
+                  label: item.name
+                }
+              })
+            }
+          })
+        }
+      })
+      this.flowerArr = res[1].data.data
+    })
   }
 }
 </script>
