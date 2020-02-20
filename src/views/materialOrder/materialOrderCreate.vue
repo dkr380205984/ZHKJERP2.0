9de6a76b705942be1ef7823f2306e94f93d9a992
dkr380205984 <380205984@qq.com>
2020-02-20 11:27:09 +0800

    导入功能好了

--- src/views/materialOrder/materialOrderCreate.vue
+++ src/views/materialOrder/materialOrderCreate.vue
@@ -15,6 +15,26 @@
         <zh-message :msgSwitch="msgSwitch"
           :url="msgUrl"
           :content="msgContent"></zh-message>
+        <div style="float:right;display:flex">
+          <span style="float:left">
+            <a class="btn noBorder"
+              style="text-decoration:none; "
+              href="https://zhihui.tlkrzf.com/1582167733000.xlsx"
+              download>下载标准模板</a>
+          </span>
+          <el-upload :auto-upload="false"
+            :on-change="elInFile"
+            :show-file-list="false"
+            action="noAction"
+            ref="upload"
+            class="upload-demo"
+            accept=".xlsx, .xls">
+            <div slot="trigger"
+              class="btn btnBlue"
+              style="height: 32px;line-height: 32px;"
+              plain>导入excel</div>
+          </el-upload>
+        </div>
       </div>
       <div class="editCtn hasBorderTop">
         <div class="normalTb"
@@ -169,11 +189,13 @@
 </template>
 
 <script>
+import XLSX from 'xlsx'
 import { client, yarn, yarnColor, materialOrder } from '@/assets/js/api.js'
 export default {
   data () {
     return {
       loading: true,
+      files: null,
       msgSwitch: false,
       msgUrl: '',
       msgContent: '',
@@ -285,6 +307,101 @@ export default {
           }
         }
       })
+    },
+    // 导入excel百度版
+    inFile (e) {
+      this.files = (e.target || e.srcElement).files // 获取"input-file"的FileList对象
+      if (!this.files || !this.files.length) { return }
+      for (let i = 0, len = this.files.length; i < len; i++) { this.read(this.files[i]) }
+    },
+    elInFile (f, fs) {
+      this.read(f.raw)
+    },
+    read (f) {
+      let rd = new FileReader()
+      rd.onload = e => { // this.readAsArrayBuffer函数内，会回调this.onload函数。在这里处理结果
+        let sheets = rd.read2JS()
+        for (let name in sheets) {
+          sheets[name].forEach((item) => {
+            this.material_info.push({
+              material_name: item['原料名称'],
+              color_code: item['原料颜色'],
+              price: item['单价'],
+              weight: item['数量']
+            })
+          })
+        }
+        this.$message.success('导入成功，订购总量不显示的情况下只要删除第一项即可')
+      }
+      rd.readAsBinaryString(f)
+    }
+  },
+  beforeCreate () {
+    FileReader.prototype.read2workbook = function () {
+      let redABS = false // 是否将文件读取为二进制字符串
+      let bytes = new Uint8Array(this.result) // 无符号整型数组
+      let wb
+      if (redABS) {
+        // let fix = new TextDecoder(encode || 'UTF-8').decode(bytes);
+        let fix = fixdata(bytes)
+        let b2a = btoa(fix) // js原生方法：将字符转为base64编码。对应的atob(base64)方法，将base64转字符
+        wb = XLSX.read(b2a, {
+          type: 'base64'
+        })
+      } else {
+        let len = bytes.byteLength
+        let binarys = new Array(len) // 创建定长数组，存储文本
+        for (let i = 0; i < len; i++) { binarys[i] = String.fromCharCode(bytes[i]) }
+        let binary = binarys.join('')
+        wb = XLSX.read(binary, {
+          type: 'binary'
+        })
+      }
+      return wb // workbook
+    }
+    FileReader.prototype.read2 = function (format = 'json') {
+      let wb = this.read2workbook()
+      if (!wb) { return null }
+
+      let r = {}
+      let formats = ['json', 'formulae', 'html', 'txt', 'csv', 'dif', 'slk', 'eth'] // 可被解析的格式
+      format = formats.indexOf(format) === -1 ? 'json' : format
+      wb.SheetNames.forEach(name => { // 遍历每张纸数据
+        r[name] = XLSX.utils['sheet_to_' + format](wb.Sheets[name])
+      })
+      return r
+    }
+    /**
+   * 解析为JSON
+   * @description: 此解析方式，XLSX将使用表格首行的每列名称，当作sheet数组对象的Key。
+   * 故Excel每个工作表格的第一行必须有表头，且名称不可重复。
+   */
+    FileReader.prototype.read2JS = function () {
+      return this.read2()
+    }
+    /* 重写readAsBinaryString函数 */
+    FileReader.prototype.readAsBinaryString = function (f) {
+      if (!this.onload) { // 如果this未重写onload函数，则创建一个公共处理方式
+        this.onload = e => { // 在this.onload函数中，完成公共处理
+          let rs = this.read2workbook()
+          console.log(rs)
+        }
+      }
+      this.readAsArrayBuffer(f) // 内部会回调this.onload方法
+    }
+
+    /**
+   * char值转String
+   * @param data {@link Array}：char值；
+   * @return {@link String}
+   */
+    function fixdata (data) {
+      let w = 1024 << 10 // 每次读取1M字节
+      let len = Math.floor(data.byteLength / w)
+      let o = new Array(len)
+      for (var i = 0; i < len; i++) { o[i] = String.fromCharCode.apply(null, new Uint8Array(data.slice(i * w, (i + 1) * w))) }
+      o[i] = String.fromCharCode.apply(null, new Uint8Array(data.slice(i * w)))
+      return o.join('')
     }
   },
   mounted () {
@@ -297,11 +414,11 @@ export default {
           value: item.name
         }
       })
-      this.colorArr = res[2].data.data.map((item) => {
+      this.colorArr = [{ value: '白胚' }].concat(res[2].data.data.map((item) => {
         return {
           value: item.name
         }
-      })
+      }))
       this.loading = false
     })
   }
